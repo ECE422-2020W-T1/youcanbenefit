ffe1b8ac3d76b5829d8bbfb565c39e0d657c7881
Cody Gramlich <cmgramli@ualberta.ca>
2020-03-31 17:40:19 -0600

    update deliver.sh

--- scripts/deliver.sh
+++ scripts/deliver.sh
@@ -7,7 +7,7 @@
 # echo 'of the previously run process (i.e. "npm start") and writes this value to'
 # echo 'the file ".pidfile".'
 # set -x
-ng serve --host 0.0.0.0
+npm run start --verbose
 # sleep 1
 # echo $! > .pidfile
 # set +x
