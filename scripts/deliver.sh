f530436a3dc306db7598db1deb1844570d128aff
Cody Gramlich <cmgramli@ualberta.ca>
2020-03-31 17:17:29 -0600

    change host to 0.0.0.0

--- scripts/deliver.sh
+++ scripts/deliver.sh
@@ -7,7 +7,7 @@
 # echo 'of the previously run process (i.e. "npm start") and writes this value to'
 # echo 'the file ".pidfile".'
 # set -x
-npm run start --verbose
+ng serve --host 0.0.0.0
 # sleep 1
 # echo $! > .pidfile
 # set +x
